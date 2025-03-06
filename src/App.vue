77ca7a2c02c236b23c5ae681a957e8e689d9ab6b
mvnribeiro <marvynribeiro@gmail.com>
2025-03-06 17:48:19 -0300

    chore: initial setup

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup>
+import ModuleAccordion from './components/ModuleAccordion.vue'
+</script>
+
+<template>
+  <main>
+    <ModuleAccordion />
+  </main>
+</template>
+
+<style scoped>
+</style>
